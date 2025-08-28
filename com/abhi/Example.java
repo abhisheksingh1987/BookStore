de04c05f6ed53302d6c999d642e952d6c4d2b89b
Abhishek Singh Java Developer <79374318+abhisheksingh1987@users.noreply.github.com>
2025-08-29 02:39:42 +0530

    Update Example.java
    Github Desktop Commit

--- com/abhi/Example.java
+++ com/abhi/Example.java
@@ -1,10 +1,10 @@
 
 package com.abhi;
 public class Example {
-
-
     public static void main (String [] args){
+
         System.out.println("I am Abhishek");
+        System.out.println("Github Desktop is Perfect");
 
     }
 }
