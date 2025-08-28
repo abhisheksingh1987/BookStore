4d8149e6846ee363d30950cedf9310a5bb61b8c4
Abhishek Singh Java Developer <79374318+abhisheksingh1987@users.noreply.github.com>
2025-08-29 03:09:09 +0530

    Space Update

--- com/abhi/Example.java
+++ com/abhi/Example.java
@@ -2,7 +2,6 @@
 package com.abhi;
 public class Example {
     public static void main (String [] args){
-
         System.out.println("I am Abhishek");
 
         System.out.println("Github Desktop is Perfect");
